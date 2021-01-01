e152686189b3b8ef69cd363765385fd5433425ae
Vishnu Akhil <vishnu.akhil10@gmail.com>
2021-01-01 12:17:06 +1100

    Initial Commit

---
+++ MoonShot - Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoonShot - Project
+//
+//  Created by Vishnu akhil Upparapalle on 1/1/21.
+//  Copyright © 2021 Vishnu akhil Upparapalle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
